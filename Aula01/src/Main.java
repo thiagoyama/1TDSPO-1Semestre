5fd082eb7b63cb6e5928c85c74d94daa346b32f2
Thiago Yamamoto <thiagoyama@gmail.com>
2024-03-01 22:49:57 -0300

    Java

---
+++ Aula01/src/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Olá alunos FIAP!");
+
+        //Criar variáveis do tipo primitivo
+        //Declaração: tipoDaVariavel nomeVariavel = valor que será atribuido
+        int idade = 20;
+        float notaChekpoint = 8.5f; //f -> define o valor como do tipo float
+        boolean aprovado = false; //valores possíveis: true ou false
+        char turma = 'O'; //Aspas simples para definir o caractere
+
+        //Criar variáveis do tipo de referência
+        String nomeCachorro = "Bili"; //String é definido com aspas duplas
+        String curso = "Análise e Desenvolvimento de sistemas";
+
+        //Exibir os dados
+        System.out.println(idade); //sout -> atalho para criar o comando System.out.println()
+        System.out.println(notaChekpoint);
+        System.out.println(aprovado);
+        System.out.println(turma);
+        System.out.println(nomeCachorro);
+        System.out.println(curso);
+
+    }//main
+}//class
